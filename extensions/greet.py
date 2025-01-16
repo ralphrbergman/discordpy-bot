7b8965426be4d5a4bd172af59b9ccefef482c0de
Ralph Rainer Bergman <ralfs@esmuralfs.lv>
2025-01-16 22:59:15 +0200

    Added a Welcoming event

--- extensions/greet.py
+++ extensions/greet.py
@@ -1,7 +1,7 @@
 from random import choice
 from typing import Optional, Tuple
 
-from discord import Member
+from discord import Member, TextChannel
 from discord.ext.commands import Cog, Bot, Context
 from discord.ext.commands import hybrid_command
 
@@ -19,6 +19,24 @@ class GreetingCog(Cog):
     def __init__(self, bot: Bot):
         self.bot = bot
     
+    @Cog.listener()
+    async def on_member_join(self, member: Member) -> None:
+        # if member.bot:  return
+
+        channels = self.bot.permissive_channels(member.guild)
+        channel: TextChannel | None
+
+        try:
+            channel = channels[0]
+        except IndexError:
+            # TODO:
+            # Handle a Guild not having any Text Channels
+            # Most likely it would just be an error in logs
+            # which will be implemented, and the logging, after this commit
+            pass
+
+        await channel.send(f"Welcome to the server, {member.mention}")
+
     @hybrid_command()
     async def greet(self, ctx: Context, person: Optional[Member] = None) -> None:
         if not person:  person = ctx.author
